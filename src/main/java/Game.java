bfa68d091b140499cbdf316b910d8c7f187f7bae
RydCri <107228115+RydCri@users.noreply.github.com>
2025-03-27 18:40:25 -0500

    Game init

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -1,72 +1,14 @@
-import javax.swing.*;
 import java.awt.*;
 
-// This code was originally going to be multiplayer game, don't let any leftover boilerplate confuse you
 public class Game extends Canvas implements Runnable {
-    private static final long serialVersionUID = 1L;
 
+    private static final long serialVersionUID = 1L;
     public static final int WIDTH = 160;
     public static final int HEIGHT = WIDTH / 12 * 9;
     public static final int SCALE = 3;
-    public static final String NAME = "Ping_Pong";
+    public static final String NAME = "Game";
 
-    private JFrame frame;
+    public void run() {
 
-    public boolean running = false;
-
-
-    private GameClient socketClient;
-    private GameServer socketServer;
-
-    public Game() {
-        setMinimumSize(new Dimension(WIDTH * SCALE, HEIGHT * SCALE));
-        setMaximumSize(new Dimension(WIDTH * SCALE, HEIGHT * SCALE));
-        setPreferredSize(new Dimension(WIDTH * SCALE, HEIGHT * SCALE));
-
-        frame = new JFrame(NAME);
-
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setLayout(new BorderLayout());
-
-        frame.add(this, BorderLayout.CENTER);
-        frame.pack();
-
-        frame.setResizable(false);
-        frame.setLocationRelativeTo(null);
-        frame.setVisible(true);
-        // socket start
-        socketClient = new GameClient(this, "localhost");
-        socketClient.start();
-
-    }
-
-    public synchronized void start() {
-        running = true;
-        new Thread(this).start();
-
-        int dialogue = JOptionPane.showConfirmDialog(this, "Start Server?");
-        if (dialogue == 0 ) {
-            socketServer = new GameServer(this);
-            socketServer.start();
-            System.out.println("Server Running." + " Thread ID: " + socketServer.threadId());
-        }
-
-        socketClient = new GameClient(this, "localhost");
-        socketClient.start();
-        System.out.println("Client Running." + " Thread ID: " + socketClient.threadId());
-    }
-
-    public synchronized void stop() {
-        running = false;
-    }
-
-    // a run method is required by the Runnable interface
-    public void run() {}
-
-    public static void main(String[] args){
-        Game pingPong = new Game();
-        pingPong.start();
-        pingPong.socketClient.sendPacket("ping".getBytes());
     }
 }
-
