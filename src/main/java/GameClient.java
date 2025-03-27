ce1365f689085b4312a7bf6f1a864d029e44b300
RydCri <107228115+RydCri@users.noreply.github.com>
2025-03-26 23:16:12 -0500

    GameClient todos

--- src/main/java/GameClient.java
+++ src/main/java/GameClient.java
@@ -7,5 +7,13 @@ public class GameClient {
     private InetAddress IpAddress;
     private DatagramSocket socket;
 
+    public Game game;
+
+    public GameClient(Game game, String IpAddress) {
+        this.game = game;
+        this.IpAddress = InetAddress.IpAddress;
+        //todo: Socket + Host Exception
+        // void run()
+    }
 
 }
