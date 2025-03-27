ba240d8ce42f6d9db46c611b7da80adb930ff8bc
RydCri <107228115+RydCri@users.noreply.github.com>
2025-03-26 23:17:14 -0500

    Canvas boilerplate

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -1,4 +1,30 @@
+import javax.swing.JFrame;
+import javax.swing.plaf.basic.BasicListUI;
 import java.awt.BorderLayout;
 import java.awt.Canvas;
+import java.awt.image.BufferedImage;
+import java.awt.image.DataBufferInt;
+
 public class Game extends Canvas implements Runnable {
+    private static final long serialVersionUID = 1L;
+
+    public static final int WIDTH = 160;
+    public static final int HEIGHT = WIDTH / 12 * 9;
+    public static final int SCALE = 3;
+    public static final String NAME = "Game";
+
+    private JFrame frame;
+
+    public boolean running = false;
+    public int tickCount = 0;
+
+    private BufferedImage image = new BufferedImage(WIDTH, HEIGHT, BufferedImage.TYPE_INT_RGB);
+    private int[] pixels = ((DataBufferInt) image.getRaster().getDataBuffer()).getData();
+    private int[] colors = new int[7 * 7 * 7];
+
+    private Screen screen;
+    public BasicListUI.MouseInputHandler input;
+    public Level level;
+    public Player player;
+
 }
