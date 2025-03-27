1def4b45619af98bbbcb4547fc7da93f0332fe68
RydCri <107228115+RydCri@users.noreply.github.com>
2025-03-26 22:44:38 -0500

    GameClient init

---
+++ src/main/java/GameClient.java
@@ -0,0 +1,11 @@
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketException;
+import java.net.UnknownHostException;
+
+public class GameClient {
+    private InetAddress IpAddress;
+    private DatagramSocket socket;
+
+
+}
